7586cf3476d10f039c52c5a9dbadc66b3450a4d9
kvalerio19 <kvalerio19@hotmail.es>
2021-06-15 15:59:30 -0600

    final

--- script.js
+++ script.js
@@ -8,105 +8,130 @@ const paddleH = 10;
 const paddleD = boardY - paddleH;
 const paddleW = 150;
 
-
 let paddleX = 150;
 let ballX = 150;
 let ballY = 150;
-let ballDx = 2;
-let ballDy = 4;
+let randomGame = [-4,-3,-2,2,3,4]
 
-let score = 0;
+//let score = 0;
 
 
-function drawGameCanvas() {
+function randomG(array){
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+let ballDX = randomG(randomGame);
+let ballDY = randomG(randomGame);
+
+
+const gameWrapper = document.getElementById('game');
+const score = document.createElement('div');
+score.className = 'score';
+score.textContent = '0';
+gameWrapper.appendChild(score);
+
+let countScore = 0;
+
+function updateDisplay(){
+    score.textContent = countScore;
+};
+function drawGameCanvas(){
     canvas = document.getElementById('gameBoard');
 
-    if (canvas.getContext) {
+    if (canvas.getContext){
         context = canvas.getContext('2d');
         gameLoop = setInterval(draw, 16);
-        window.addEventListener('keydown', keyInput, true);
-
+        window.addEventListener('keydown', keyInput, true)
     }
 }
 
+function draw(){
+    context.clearRect(0,0, boardX, boardY);
 
-function draw() {
-
-    context.clearRect(0, 0, boardX, boardY);
-
-    context.fillStyle = 'thistle';
+    context.fillStyle = 'lightsteelblue';
     context.beginPath();
     context.rect(0, 0, boardX, boardY);
     context.closePath();
     context.fill();
 
-    context.fillStyle = 'tomato';
+    context.fillStyle = 'salmon';
     context.beginPath();
     context.arc(ballX, ballY, 15, 0, Math.PI * 2, true);
     context.closePath();
     context.fill();
 
-    context.fillStyle = 'navy';
+    context.fillStyle = 'steelblue';
     context.beginPath();
     context.rect(paddleX, paddleD, paddleW, paddleH);
     context.closePath();
     context.fill();
 
-    ballX += ballDx;
-    ballY += ballDy;
+    ballX += ballDX;
+    ballY += ballDY;
 
-
-    if (ballX + ballDx > boardX - 15 || ballX + ballDx < 15) {
-        ballDx = -ballDx;
-
+    if (ballX + ballDX > boardX - 15 || ballX + ballDX < 15){
+        ballDX = -ballDX;
     }
-    if (ballY + ballDy < 10) {
-        ballDy = -ballDy;
 
-    } else if (ballY + ballDx > boardY - 15) {
-        if (ballX > paddleX && ballX < paddleX + paddleW) {
-            ballDy = -ballDy;
-            score++; // Collinsion Detection
-        } else {
+    if(ballY + ballDY < 15) {
+        ballDY = -ballDY;
+    } else if (ballY + ballDY > boardY -15) {
+        if(ballX > paddleX && ballX < paddleX + paddleW){
+            ballDY = -ballDY;
+            countScore++;
+            updateDisplay();
+        }else {
             clearInterval(gameLoop);
             //alert('Game Over');
             //swal("Oops!", "Game Over!");
             //displayMessage('Ocurrio un Error', 'warning');
-            error = "GAME OVER";
-            document.getElementById("gameOver").innerHTML = error;
-
+            gameOver();
         }
-        fill('#ffff');
-        textSize(24);
-        text('Score:' + score, 10, 25);
-
+        //fill('#ffff');
+        //textSize(24);
+        //text('Score:' + score, 10, 25);
     }
-
-
 }
-
-
+function gameOver(){
+    const alertDIV = document.createElement('div');
+    const reset = document.createElement('button');
+    reset.textContent = 'Reset';
+    alertDIV.className = 'gameOver';
+    gameWrapper.appendChild(alertDIV);
+    gameWrapper.appendChild(reset);
+    alertDIV.textContent = "Game Over";
+    reset.onclick = function(){
+        gameWrapper.removeChild(reset);
+        gameWrapper.removeChild(alertDIV);
+        countScore = 0;
+        updateDisplay();
+        ballX = 150;
+        ballY = 150;
+        gameLoop = setInterval(draw, 16);
+        ballDX = randomG(randomGame);
+        ballDY = randomG(randomGame);
+    }
+}
 function keyInput(e) {
     switch (e.keyCode) {
         case 37:
             paddleX -= 20;
-            if (paddleX < 0) {
+            if (paddleX < 0){
                 paddleX = 0;
             }
             break;
         case 39:
-            paddleX += 20;
-            if (paddleX > boardX - paddleW) {
+            paddleX += 20; 
+            if (paddleX > boardX - paddleW){
                 paddleX = boardX - paddleW;
             }
             break;
     }
 }
 
-function resetGame() {
+/*function resetGame() {
     window.location.reload()
 
-}
-
+}*/
 
 drawGameCanvas();
